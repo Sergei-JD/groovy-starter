5f1cc2dbd9ca11a6cabd4f70685fcaf1c5eb36fc
Sergei-JD <sergshlyazhko@gmail.com>
2022-02-03 18:27:43 +0300

    added Closure

---
+++ src/com/groovy/starter/lesson25/Closure2Runner.groovy
@@ -0,0 +1,28 @@
+package com.groovy.starter.lesson25
+
+import com.groovy.starter.lesson17.Student
+
+class Closure2Runner {
+
+    static void main(String[] args) {
+        Closure closure = {
+            firstName = "Sveta"
+        }
+        println closure.thisObject
+        println closure.owner
+        println closure.delegate
+
+        def student = new Student("Ivan", "Ivanov", 29)
+        println student
+
+//        closure.delegate = student
+//        closure.resolveStrategy = Closure.DELEGATE_FIRST
+//        closure()
+
+        student.with closure
+
+        println student
+
+        student.getInfo()()
+    }
+}
